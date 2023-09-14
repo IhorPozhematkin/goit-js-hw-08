83208fd216715ac57dfdd1441800455374c8855b
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-09-14 15:38:13 +0300

    js-hw-08-2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+let saveForm = {
+    email: '',
+    message: '',
+  };
+form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+onHoldText();
+function onInput(e) {
+  saveForm = {
+    email: input.value,
+    message: textarea.value,
+  };
+  localStorage.setItem(KEY, JSON.stringify(saveForm));
+}
+function onSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY);
+  console.log(saveForm);    
+}
+function onHoldText() {
+  const holdText = localStorage.getItem(KEY);
+  if (holdText) {
+    saveForm = JSON.parse(holdText);
+    input.value = saveForm.email;
+    textarea.value = saveForm.message;
+  };
+}
